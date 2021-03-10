4dc319cd9c5300455e3541fbb7d9ce59789b92b0
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-03-11 00:05:16 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework2
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] points;
+        T2[] films;
+        public MyDictionary()
+        {
+            points = new T1[0];
+            films = new T2[0];
+        }
+        public void Add(T1 point,T2 film)
+        {
+            T1[] tempArrayp = points;
+            T2[] tempArrayf = films;
+            points = new T1[points.Length + 1];
+            films = new T2[films.Length + 1];
+            for (int i = 0; i < tempArrayf.Length; i++)
+            {
+                points[i] = tempArrayp[i];
+                films[i] = tempArrayf[i];
+
+            }
+            points[points.Length - 1] = point;
+            films[films.Length - 1] = film;
+        }
+        public void Listing()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine(points[i] + " puanlı film " + films[i]);
+            }
+           
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kubilay TUNÇBAŞ'ın Film Puanlaması");
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(10, "The Lord Of The Rings:Two Towers");
+            myDictionary.Add(8, "The Godfather");
+            myDictionary.Add(8, "Pulp Fiction");
+            myDictionary.Add(7, "Harry Potter");
+            myDictionary.Add(8, "Star Wars:Part 2");
+
+            myDictionary.Listing();
+            
+        }
+    }
+}
